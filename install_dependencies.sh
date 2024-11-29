fe57bfbee5324d610384a7d6267f41faadb01e36
Andrew Plotner <bluebandit21@gmail.com>
2024-11-29 17:11:37 -0500

    Do the stuff?

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -33,11 +33,11 @@ wget "https://git.morello-project.org/morello/llvm-project-releases/-/archive/mo
 tar -xzf "llvm-project-releases-morello-linux-release-1.8.tar.gz"
 
 cd llvm-project-releases-morello-linux-release-1.8
-cp -r bin/ ${script_dir}/toolchain/bin/
-cp -r include/ ${script_dir}/toolchain/include/
-cp -r lib/ ${script_dir}/toolchain/lib/
-cp -r libexec/ ${script_dir}/toolchain/libexec/
-cp -r share/ ${script_dir}/toolchain/share/
+cp -r bin/ ${script_dir}/toolchain/
+cp -r include/ ${script_dir}/toolchain/
+cp -r lib/ ${script_dir}/toolchain/
+cp -r libexec/ ${script_dir}/toolchain/
+cp -r share/ ${script_dir}/toolchain/
 
 
 export PATH="${script_dir}/toolchain/bin":"${PATH}"
@@ -63,19 +63,46 @@ ${MORELLO_SUPPORT} \
 make
 make install
 
-cd cd ${script_dir}
+cd ${script_dir}
 
 
 ## Kernel Headers
+cd "${script_dir}/../morello_workspace/linux/"
+make headers_install HOSTCC=clang CC=clang ARCH=arm64 INSTALL_HDR_PATH="${script_dir}/morello_toolchain"
 
 
 
+# Morello Linux LTP!
+cd "${script_dir}"
+wget "https://git.morello-project.org/morello/morello-linux-ltp/-/archive/morello-release-1.8.0/morello-linux-ltp-morello-release-1.8.0.tar"
+tar -xf "morello-linux-ltp-morello-release-1.8.0.tar"
+rm "morello-linux-ltp-morello-release-1.8.0.tar"
 
+cd morello-linux-ltp-morello-release-1.8.0
 
+export KHDR_DIR="${script_dir}/morello_toolchain"
+export MUSL="${script_dir}/morello_toolchain"
+export LTP_BUILD="${script_dir}/ltp_build"
+export LTP_INSTALL="${script_dir}/ltp_install"
 
+rm -rf ${LTP_BUILD}
+rm -rf ${LTP_INSTALL}
 
+$(
+    CFLAGS="--target=${TRIPLE} ${TARGET_FEATURE} --sysroot=${MUSL} \
+        -isystem ${KHDR_DIR}/usr/include -g -Wall"
+    LDFLAGS="--target=${TRIPLE} -rtlib=compiler-rt --sysroot=${MUSL} \
+         -fuse-ld=lld -static -L${LTP_BUILD}/lib"
+        export CC=clang
+    export HOST_CFLAGS="-O2 -Wall"
+    export HOST_LDFLAGS="-Wall"
+    export CONFIGURE_OPT_EXTRA="--prefix=/ --host=aarch64-linux-gnu --disable-metadata --without-numa"
 
-
+    MAKE_OPTS="TST_NEWER_64_SYSCALL=no TST_COMPAT_16_SYSCALL=no" \
+    TARGETS="pan tools/apicmds testcases/kernel/syscalls" BUILD_DIR="$LTP_BUILD" \
+    CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" \
+    ./build.sh -t cross -o out -ip "${LTP_INSTALL}"
+)
 
 
 cd ${curr_dir}
